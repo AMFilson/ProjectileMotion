3e077aada97700ba5ee7ffcfb7b5bfc0cb1c423f
AMFilson <andrew.filson13@gmail.com>
2026-03-13 07:49:19 -0400

    leaving home will continue later

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+  /*There should be a scanner to get user input from keyboard*/
+  private Scanner universalInput = new Scanner(System.in);
+
   public static void main(String[] args){
+
     /*The main game will be ran here*/
     /*There will be two players in total*/
-    /*There should be a scanner to get user input from keyboard*/
+
+
 
   }
 }
