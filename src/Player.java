10de828c650e3e885d7eb04212924785df8ccc08
AMFilson <andrew.filson13@gmail.com>
2026-03-13 14:25:34 -0400

    small additions for convenience

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.math.*;
+
 public class Player {
   /*The plan is to define a player with the requisite properties, methods, getters and setters*/
   /*There should be something to track scores between games too*/
@@ -5,11 +7,14 @@ public class Player {
   private String name;
   private int power;
   private int angle;
+  private int startingPosition;
+/*  private double shot;*/
 
   public Player(String name, int power, int angle){
     this.name = name;
     this.power = power;
-    this.angle = angle;
+    this.angle = angle;/*
+    this.shot = shot;*/
   }
 
   public String getName() {
@@ -22,6 +27,18 @@ public class Player {
 
   public int getAngle(){
     return angle;
+  }
+
+  public double getShot(){
+    final double GRAVITY = -9.81;
+    /*determine the time it takes for a shot to land*/
+   double timeToLand = (GRAVITY / (2 * getPower() * Math.sin(getAngle()));
+   /*get the x-coordinate or the shot*/
+    (getPower() * Math.cos(getAngle()) * timeToLand + ;
+  }
+
+  public void setStartingPosition(){
+    startingPosition = (int) (Math.random() * 120);
   }
 
 
