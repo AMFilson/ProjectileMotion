777f9aef866b49612a72566860d411e9e49c0c4e
AMFilson <andrew.filson13@gmail.com>
2026-03-13 07:32:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args){
+    /*The main game will be ran here*/
+    /*There will be two players in total*/
+    /*There should be a scanner to get user input from keyboard*/
+
+  }
+}
---
+++ src/Player.java
@@ -0,0 +1,5 @@
+public class Player {
+  /*The plan is to define a player with the requisite properties, methods, getters and setters*/
+  /*There should be something to track scores between games too*/
+  /*And a static method to track total amount of games played?*/
+}
