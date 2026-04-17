b992eaf320ba584ffdd270e2b81c91291918b9dd
AMFilson <andrew.filson13@gmail.com>
2026-04-17 17:56:30 -0400

    fixed accidentally saved temp files.
    appended old console UI

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,18 @@ Author:Andrew Filson
 Date: March Friday the 13th 2026!
 Desc: A game of shoot at a target
  */
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
 
   public static void main(String[] args) {
-    Scanner universalInput = new Scanner(System.in);
-    ArrayList<String> scoreBoard = new ArrayList<>();
-
-    Game game = new Game(universalInput, scoreBoard);
-    game.start();
+    /*
+     * Appended to convert to animated UI game
+     * Scanner universalInput = new Scanner(System.in);
+     * ArrayList<String> scoreBoard = new ArrayList<>();
+     * 
+     * Game game = new Game(universalInput, scoreBoard);
+     * game.start();
+     */
+    // Launch the Swing GUI
+    javax.swing.SwingUtilities.invokeLater(() -> new MainWindow());
   }
 }
