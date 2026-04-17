078be4b60a832fadbe1203792af94bc216508c12
AMFilson <andrew.filson13@gmail.com>
2026-04-17 17:50:55 -0400

    switched out what the main function loads to the GUI rather than console

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,25 @@ Author:Andrew Filson
 Date: March Friday the 13th 2026!
 Desc: A game of shoot at a target
  */
+
+/* Appended for now need to convert to a UI based game 
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Scanner; */
 
 public class Main {
 
   public static void main(String[] args) {
-    Scanner universalInput = new Scanner(System.in);
-    ArrayList<String> scoreBoard = new ArrayList<>();
 
-    Game game = new Game(universalInput, scoreBoard);
-    game.start();
+    /*
+     * Appended for now pending UI conversion
+     * Scanner universalInput = new Scanner(System.in);
+     * ArrayList<String> scoreBoard = new ArrayList<>();
+     * 
+     * Game game = new Game(universalInput, scoreBoard);
+     * game.start();
+     */
+
+    // Launch the Swing GUI
+    javax.swing.SwingUtilities.invokeLater(() -> new MainWindow());
   }
 }
