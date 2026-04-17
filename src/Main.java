57831256b7986dfc7ad03a8daa9865e5087f42cc
AMFilson <andrew.filson13@gmail.com>
2026-04-17 17:52:06 -0400

    Revert "switched out what the main function loads to the GUI rather than console"
    This reverts commit 078be4b60a832fadbe1203792af94bc216508c12.

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,16 @@ Author:Andrew Filson
 Date: March Friday the 13th 2026!
 Desc: A game of shoot at a target
  */
-
-/* Appended for now need to convert to a UI based game 
 import java.util.ArrayList;
-import java.util.Scanner; */
+import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
+    Scanner universalInput = new Scanner(System.in);
+    ArrayList<String> scoreBoard = new ArrayList<>();
 
-    /*
-     * Appended for now pending UI conversion
-     * Scanner universalInput = new Scanner(System.in);
-     * ArrayList<String> scoreBoard = new ArrayList<>();
-     * 
-     * Game game = new Game(universalInput, scoreBoard);
-     * game.start();
-     */
-
-    // Launch the Swing GUI
-    javax.swing.SwingUtilities.invokeLater(() -> new MainWindow());
+    Game game = new Game(universalInput, scoreBoard);
+    game.start();
   }
 }
